990ae4c9ca112cf6bd7f4aa02baa48d29e34b2cd
rendnil <andrew.lindner.15@gmail.com>
2019-01-03 16:51:39 -0500

    updated gitignore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+  componentDidMount(){
+    fetch(process.env.REACT_APP_TEST)
+    .then(r=>r.json())
+    .then(data=>console.log(data))
+  }
+
+
   render() {
+    console.log(process.env.REACT_APP_KEY);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+
+
       </div>
     );
   }
